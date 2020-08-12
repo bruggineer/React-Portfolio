09d22ca1fc2fff0533cdcff82833bccfd6ff5f73
TBruggeman <tsbruggeman@gmail.com>
2020-08-12 15:45:52 -0700

    portfolio to react

---
+++ public/jinx.js
@@ -0,0 +1,40 @@
+<div class="container">
+    <div class="navbar navbar-default">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Bootstrap 3</a>
+        </div>
+
+
+        <div class="jumbotron">
+            <h1>Twitter Bootstrap 3.0</h1>
+            <p class="lead">Starter template with CSS and JS included.</p>
+            <p><a class="btn btn-lg btn-primary" href="http://yahoo.fr" target="_blank">link</a></p>
+        </div>
+        <div class="area">
+            <span class="thezone" id="zone1" data-toggle="modal" data-target="#myModal">HERE</span> i want to put the modal just above this word and still be able to see the word "HERE"
+        </div>
+        <div class="area2">
+            <span class="thezone2" id="zone2" data-toggle="modal" data-target="#myModal"></span>
+        </div>
+
+        <div class="modal in" id="notificationModal" role="dialog" aria-labelledby="notificationModalLabel" aria-hidden="true" data-backdrop="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title" id="notificationModalLabel" style="color:#DF2943;">
+                            How to take part in the deal?
+                        </h3>
+                    </div>
+                    <div class="modal-body">
+                        <h4 style="margin-top:0px">
+                            some explanations
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="push"></div>
