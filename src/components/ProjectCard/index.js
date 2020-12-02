3ffbb2a9bf44fd671cb154189ef7d32493f0dab3
TBruggeman <tsbruggeman@gmail.com>
2020-12-02 11:31:26 -0800

    working on ProjectCards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,6 +1,4 @@
-import React, { Component, useState } from "../../../node_modules/react";
-import { render } from "../../../node_modules/react-dom";
-import SlidingPane from "../../../node_modules/react-sliding-pane";
+import React from "../../../node_modules/react";
 import "../../../node_modules/react-sliding-pane/dist/react-sliding-pane.css";
 import Burgers from "../../assets/images/Burgers.jpg"
 import ExpenseTracker from "../../assets/images/ExpenseTracker.png";
@@ -9,8 +7,10 @@ import LootTracker from "../../assets/images/LootTracker-small.png";
 import Passwordgenerator from "../../assets/images/Passwordgenerator.png";
 import Shuffle from "../../assets/images/shuffle2.png";
 import WeatherDash from "../../assets/images/WeatherDash.png";
-import { Card, Row } from "../../../node_modules/reactstrap";
-import { Container } from '../../../node_modules/reactstrap';
+import { Card, Container } from "../../../node_modules/reactstrap";
+import { IconContext } from "react-icons";
+import { FaGithubAlt, FaExternalLinkSquareAlt, FaChevronCircleRight } from 'react-icons/fa';
+import Image from 'react-bootstrap/Image'
 import "./style.css";
 
 export default function ProjectCard(props) {
@@ -30,19 +30,25 @@ export default function ProjectCard(props) {
     <Container id="portfolio-main" className="bg-dark" attr={props.id} data-section-name={props.img}>
       <Card bg="dark" text="white">
         <div className="img-box bg-dark product">
-          <img className="card-img align-middle bg-dark" alt={props.title} src={images[props.img]} />        </div>
-        <div id="slide" className="intro bg-dark">
-            <div className="description card text-white bg-dark">
-                <h2>{props.title}</h2>
-                <p className="card-text">{props.blurb}</p>
-                <p className="small">{props.team}</p>
-                <p><a href={props.site} target="__blank">See App in Action</a></p>
-                <p><a href={props.repo} target="__blank">Project Repository</a></p>
-                <span className="viewmore">+</span>
+          <Image className="card-img align-middle bg-dark" alt={props.title} src={images[props.img]} fluid />
+        </div>
+        <div className="intro bg-dark">
+          <div className="description card text-white bg-dark">
+            <h2>{props.title}</h2>
+            <p className="card-text">{props.blurb}</p>
+            <p className="small">{props.team}</p>
+            <div>
+            <IconContext.Provider value={{color: "#fff", size: "4em", style: { padding: "2rem" }, className: "link"}}>
+              <a href={props.site} target="__blank" className="link"><FaExternalLinkSquareAlt /></a>
+              <a href={props.repo} target="__blank" className="link"><FaGithubAlt /></a>
+            </IconContext.Provider>
             </div>
+          </div>
+          <div className="more">
+            <FaChevronCircleRight/>
+          </div>         
         </div>
       </Card>
     </Container>
   );
 }
-      
