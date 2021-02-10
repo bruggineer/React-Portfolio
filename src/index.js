241bd6144aee3375ed7fd0c62cf5b4e4f64080c8
bruggineer <tsbruggeman@gmail.com>
2021-02-10 12:57:21 -0800

    added .env for public_url

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style.css';
 import App from './App';
-  
+
+console.log('public url: ', process.env.PUBLIC_URL)
+
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
