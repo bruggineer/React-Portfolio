7476a833c98d86338281121eb758a41f3244bdf1
bruggineer <tsbruggeman@gmail.com>
2021-02-10 12:57:21 -0800

    added .env for public_url

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import { Link } from "../../../node_modules/react-router-dom";
 import "./style.css";
 
 function Nav() {
-  return (
-      <nav className="navbar navbar-expand-lg navbar-dark">
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark">
             <Link className="navbar-brand" to="/">Teresa Bruggeman</Link>
             <button className="navbar-toggler btn-light justify-content-end" type="button" data-toggle="collapse"
                 data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,13 +13,13 @@ function Nav() {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav float-right">
                     <li className="nav-item">
-                        <Link to='/' className={window.location.pathname ==='/' || window.location.pathname === '/home' ? "nav-link active" : "nav-link"} >Bio</Link>
+                        <Link to='/' className={window.location.pathname === '/' || window.location.pathname === '/home' ? "nav-link active" : "nav-link"} >Bio</Link>
                     </li>
                     <li className="nav-item">
                         <Link to='/portfolio' className={window.location.pathname === '/portfolio' ? "nav-link active" : "nav-link"} >Portfolio</Link>
-                     </li>
+                    </li>
                     <li className="nav-item">
-                    <Link to='/contact' className={window.location.pathname === '/contact' ? "nav-link active" : "nav-link"} >Contact</Link>
+                        <Link to='/contact' className={window.location.pathname === '/contact' ? "nav-link active" : "nav-link"} >Contact</Link>
                     </li>
                 </ul>
             </div>
