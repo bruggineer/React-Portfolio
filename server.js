7b7da210c8d876dd06eea81d92fa0df160356a5a
bruggineer <tsbruggeman@gmail.com>
2021-02-24 18:33:09 -0800

    using gh-pages to publish at GitHub Pages, it is rendering both locally and on GitHub Pages

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
-//app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("../public"));
 
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "..", "build")));
+//app.use(express.static("../public"));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port);
