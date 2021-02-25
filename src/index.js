6e616677e76d99810e8e33aa018528d248c68a5f
bruggineer <tsbruggeman@gmail.com>
2021-02-24 18:50:56 -0800

    Reverting to c5eeebd, prior to overwriting master with gh-pages build

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './assets/style.css';
 import App from './App';
 
+console.log('public url: ', process.env.PUBLIC_URL)
+
 ReactDOM.render(<App />, document.getElementById("root"));
