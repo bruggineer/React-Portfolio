b1454160c6a5f6017d22aafb46c43d9030e4ba52
bruggineer <tsbruggeman@gmail.com>
2021-02-24 18:13:49 -0800

    Updates

--- src/components/Container/index.js
+++
@@ -1,7 +0,0 @@
-import React from "../../../node_modules/react";
-
-function Container(props){
-    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
-}
-
-export default Container;
