9f3bb7e1866eb6a89cb66672e5d09ea397e080c0
bruggineer <tsbruggeman@gmail.com>
2021-04-10 09:29:47 -0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/style.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
