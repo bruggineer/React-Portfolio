56cb62bb17b39dac970989f0f1e231acdc96ba93
bruggineer <tsbruggeman@gmail.com>
2021-04-12 11:18:04 -0700

    clean up for deployment

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'src', 'assets')))
 
 
-// //code for gh-pages build
-// app.use(express.static(path.join(__dirname, "..", "build")));
+//code for gh-pages build
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, "..", "build", "react-app", "index.html"));
-// });
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "react-app", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
